db36e61fd1651edd4251ae860f65e37aeeee35d2
tmlouw <tmlouw@CI507201.stb.sun.ac.za>
2022-09-22 22:35:42 +0200

    Referring to new "t" structure, which contains time step parameter and all time points.

--- Blending tank example/SensorFault.m
+++ Blending tank example/SensorFault.m
@@ -1,16 +1,18 @@
-function fs = SensorFault(fs, x, t)
+function fs = SensorFault(fs, x, r, t)
     
     % Trigger the developed fault if t > fault time and it hasn't been
     % triggered yet
-    if (t > fs.fault.time) && (~fs.fault.triggered)
+    if (t.Time(end) > fs.fault.time) && (~fs.fault.triggered)
         fs.C.state = 'Drift'; %'None', 'Stuck', 'Drift', 'Bias'
+    elseif (fs.fault.triggered) && (r.replace.C)
+        fs.C.State = 'None';    % The supervisory control flagged "C" to be fixed
     end
 
     % Adjust drift for each sensor which is currently drifting
     for i = 1: length(fs.fields)
         f = fs.fields{i};
         if strcmp(fs.(f).state, 'Drift')
-            fs.(f).drift = fs.(f).drift + fs.(f).drift_rate;
+            fs.(f).drift = fs.(f).drift + fs.(f).drift_rate*t.dt;
         end
     end
 end
