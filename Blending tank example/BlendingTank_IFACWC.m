4f1d23d54e300c14d4798b6ac86042e7d54883ce
tmlouw <tmlouw@CI507201.stb.sun.ac.za>
2022-09-22 12:13:07 +0200

    Modified variable names to match simulator description (e.g., measurement parameters now contained in "y" structure). Converted control valve parameters from "k" and "s" to "tau" and "xi". Converted state variables from liquid level and concentration to total volume and solute mass.

--- Blending tank example/BlendingTank_IFACWC.m
+++ Blending tank example/BlendingTank_IFACWC.m
@@ -10,8 +10,8 @@ t = 0:7200;    % s, over one day
 %% Parameters
 % Define process parameters
 p.A  = 0.5;     % m2, mixing tank cross-sectional area
-p.k = 1;        % 1/s, valve spring constant
-p.s = 1;        % ~, valve damping coefficient
+p.tauI = 1;     % s, valve time constant
+p.xi = 5;       % ~, valve damping coefficient
 p.K = 0.1;      % m3/kg, controller gain
 p.tauI = 10;    % s, controller time constant
 p.cv = 0.1;     % m3/s, control valve coefficient
@@ -19,7 +19,7 @@ p.kv = 0.06;    % m2.5/s, drainage valve coefficient
 
 % List of state variables. Create an empty array for each state value,
 % which will be used in the Simulate function
-p.state_fields = {'C', 'L', 'x', 'v', 'I'};
+p.state_fields = {'m', 'V', 'xv', 'v'};
 for i = 1:length(p.state_fields)
     p.x_empty.(p.state_fields{i}) = [];
 end
@@ -29,37 +29,41 @@ end
 % the measurement from the prcoess variables, as well as a noise variance
 % which specifies the variance of the normally distruted noise added to
 % each measurement
-measurement.fields = {'C','C0','F0','FW', 'F', 'L', 'KPI'};
+y.fields = {'C','C0','F0','FW', 'F', 'L', 'KPI'};
 
 % Concentration in the tank
-measurement.C.function = @(t, x, sp, d, v) x.C(end);
-measurement.C.noise_var = 0.01;
+y.C.function = @(t, x, sp, d, v) x.C(end);
+y.C.noise_var = 0.01;
 
 % Inlet concentration
-measurement.C0.function = @(t, x, sp, d, v) d.C0(t);
-measurement.C0.noise_var = 0.01;
+y.C0.function = @(t, x, sp, d, v) d.C0(t);
+y.C0.noise_var = 0.01;
 
 % Inlet flowrate
-measurement.F0.function = @(t, x, sp, d, v) d.F0(t);
-measurement.F0.noise_var = 0.002;
+y.F0.function = @(t, x, sp, d, v) d.F0(t);
+y.F0.noise_var = 0.002;
 
 % Water flowrate
-measurement.FW.function = @(t, x, sp, d, v) v.FW(end);
-measurement.FW.noise_var = 0.002;
+y.FW.function = @(t, x, sp, d, v) v.FW(end);
+y.FW.noise_var = 0.002;
 
 % Liquid level
-measurement.L.function = @(t, x, sp, d, v) x.L(end);
-measurement.L.noise_var = 0.002;
+y.L.function = @(t, x, sp, d, v) x.L(end);
+y.L.noise_var = 0.002;
 
 % Outlet flowrate
-measurement.F.function = @(t, x, sp, d, v) v.F(end);
-measurement.F.noise_var = 0.002;
+y.F.function = @(t, x, sp, d, v) v.F(end);
+y.F.noise_var = 0.002;
 
 % Key performance indicator
-measurement.KPI.function = @(t, x, sp, d, v) exp( -40*(x.C(end) - sp.C(t)).^2 );
-measurement.KPI.noise_var = 0;
+y.KPI.function = @(t, x, sp, d, v) exp( -40*(x.C(end) - sp.C(t)).^2 );
+y.KPI.noise_var = 0;
     
-%% Sensor faults
+%% Process faults (fp)
+% Initialize process fault
+fp.valve.state = 'None';
+
+%% Sensor faults (fs)
 % Each measurement has an associated fault state, 
 % which may be "None", "Bias", "Drift" or "Stuck". 
 % Each sensor also has a drift rate associated with the "Drift" fault, 
@@ -68,23 +72,23 @@ measurement.KPI.noise_var = 0;
 % "Bias" fault
 
 % Faults for concentration measurement
-sensor_fault.C.state = 'None';
-sensor_fault.C.drift = 0;
-sensor_fault.C.drift_rate = 0.0001;
-sensor_fault.C.bias = 0.2;
+fs.C.state = 'None';
+fs.C.drift = 0;
+fs.C.drift_rate = 0.0001;
+fs.C.bias = 0.2;
 
 % All other faults; none will be introduced for this example
-sensor_fault.C0.state = 'None';
-sensor_fault.F0.state = 'None';
-sensor_fault.FW.state = 'None';
-sensor_fault.F.state = 'None';
-sensor_fault.L.state = 'None';
-sensor_fault.KPI.state = 'None';
+fs.C0.state = 'None';
+fs.F0.state = 'None';
+fs.FW.state = 'None';
+fs.F.state = 'None';
+fs.L.state = 'None';
+fs.KPI.state = 'None';
 
 %% Monitoring parameters
-monitor.nComponents = 2;
-monitor.T2_threshold = 30;
-monitor.SPE_threshold = 20;
+m.nComponents = 2;
+m.T2_threshold = 30;
+m.SPE_threshold = 20;
 
 %% Disturbance variables
 % Create stochastic inlet flowrate and concentrations over time
@@ -102,21 +106,19 @@ clear F0 C0
 
 %% Set-point changes
 % Specify set-point changes
-sp.C = @(t) 0.5 + 0.*(t>(3600*0.5));
+r.C = @(t) 0.5 + 0.*(t>(3600*0.5));
 
-%% Startup
-% Define the startup procedure, which occurs initially and after
-% maintenance shut downs
-startup.C = 0.5; % kg/m3, initial concentration in tank
-startup.L = 1;   % m, initial liquid level
-startup.x = 0.5; % ~, initial fraction valve opening
-startup.v = 0;   % 1/s, initial valve velocity
-startup.I = 0;   % kg.s/m3, initial integral error
+%% Initialize
+% Initialize the process variables
+x.m = 0.5; % kg, initial solute concentration in tank
+x.V = 1;   % m3, initial liquid volume in tank
+x.xv = 0.5; % ~, initial fraction valve opening
+x.v = 0;   % 1/s, initial valve velocity
+x = intermediateVariables(x, u, d, fp, p);
 
 %% Integrate ODEs
 % Initialize the simulation
-x = startup;
-y = initMeasurement(x, sp, d, p, measurement); % Initialize measurements
+%y = initMeasurement(x, sp, d, p, y); % Initialize measurements
 shut = [];  % s, vector containing time values where plant was shut down
 fault_time = 2185;
 
@@ -125,27 +127,27 @@ disp('Simulation started')
 i = 1;
 while t(i) < t(end)
     i = i+1;
-    [x, v] = Simulate(t(i-1:i), x, y, sp, d, p);
-    [y, sensor_fault] = updateMeasurement(t(i), x, v, y, sp, d, p, measurement, sensor_fault);
+    x = Simulate(x, u, d, fp, t(i-1:i), p);
+    [y, fs] = updateMeasurement(t(i), x, v, y, sp, d, p, y, fs);
     
     % Monitoring related activities
     if t(i) == 2000 % Train monitoring model
-        [T, T2, SPE, faulty, monitor] = initMonitoring(y, monitor);
+        [T, T2, SPE, faulty, m] = initMonitoring(y, m);
         
     elseif t(i) > 2000 % Continue with monitoring
-        [T(i,:), T2(i), SPE(i), faulty(i)] = Monitoring(y, monitor);
+        [T(i,:), T2(i), SPE(i), faulty(i)] = Monitoring(y, m);
 
         if (sum(faulty(i-59:i)) / 60) > 0.8
             disp('Shut down');
             shut = [shut t(i)];
-            sensor_fault.C.state = 'None';
+            fs.C.state = 'None';
         end
     end
     
     % Fault related activities
     % Trigger a sensor fault
     if (t(i) > fault_time) && isempty(shut)
-        sensor_fault.C.state = 'Drift'; %'None', 'Stuck', 'Drift', 'Bias'
+        fs.C.state = 'Drift'; %'None', 'Stuck', 'Drift', 'Bias'
     end
     disp(i/length(t))
 end
