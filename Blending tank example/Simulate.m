a17e0cebe07acb2288b9085ebbb1eda8802dfc08
tmlouw <tmlouw@CI507201.stb.sun.ac.za>
2022-09-22 12:14:40 +0200

    Converted control valve parameters from "k" and "s" to "tau" and "xi". Converted state variables from liquid level and concentration to total volume and solute mass. Removed integral error from process, to be added to measurement. Added intermediate variables to process variable structure "x". Changed ODE input from "sp" (for valve set-point") to control signal "u".

--- Blending tank example/Simulate.m
+++ Blending tank example/Simulate.m
@@ -1,4 +1,4 @@
-function [x, v] = Simulate(tspan, x, y, sp, d, p)
+function x = Simulate(x, u, d, fp, tspan, p)
 
 %     % Using ode45
 %     [~, xvec] = ode45(@(t, x) ODEs(t, x, y, sp, d, p), tspan, struct2vec(x, p));
@@ -6,34 +6,30 @@ function [x, v] = Simulate(tspan, x, y, sp, d, p)
     % Using hardcoded RK4
     dt = tspan(end) - tspan(1);     % Time step
     x0 = struct2vec(x, p);          % Initial values as a vector
-    k1 = ODEs(tspan(1), x0, y, sp, d, p);
-    k2 = ODEs(tspan(1) + dt/2, x0 + dt/2*k1, y, sp, d, p);
-    k3 = ODEs(tspan(1) + dt/2, x0 + dt/2*k2, y, sp, d, p);
-    k4 = ODEs(tspan(1) + dt, x0 + dt*k3, y, sp, d, p);
+    k1 = ODEs(tspan(1), x0, u, d, fp, p);
+    k2 = ODEs(tspan(1) + dt/2, x0 + dt/2*k1, u, d, fp, p);
+    k3 = ODEs(tspan(1) + dt/2, x0 + dt/2*k2, u, d, fp, p);
+    k4 = ODEs(tspan(1) + dt, x0 + dt*k3, u, d, fp, p);
     xvec = (x0 + dt/6*(k1 + 2*k2 + 2*k3 + k4))';    % Transpose to generate row vector, as would be done using ode45
 
     % Generate the necessary structures
     x = vec2struct(xvec(end,:), x, p);
-    v = intermediateVariables(tspan(end), x, y, sp, d, p);
-
 end
 
-function dxdt = ODEs(t, xvec, y, sp, d, p)
+function dxdt = ODEs(t, xvec, u, d, fp, p)
     x = vec2struct(xvec, p.x_empty, p);
-    v = intermediateVariables(t, x, y, sp, d, p);
 
-    ddt.C = ( d.C0(t)*d.F0(t) - x.C*v.F ) / (p.A*x.L);
-    ddt.L = (d.F0(t) + v.FW - v.F) / p.A;
-    ddt.x = x.v;
-    ddt.v = (p.k*(v.xsp - x.x) - p.s*x.v);
-    ddt.I = v.e;
+    ddt.m = d.C0(t)*d.F0(t) - x.C*x.F;
+    ddt.V = d.F0(t) + x.FW - x.F;
+    ddt.xv = x.v;
+    ddt.v = (1/p.tau^2)*(u.xv - x.xv) - 2*p.xi/p.tau*x.v;
     
     % Maintain fraction valve opening in [0, 1]
-    if (x.x == 0) && (ddt.v < 0)
-        ddt.x = 0;
+    if (x.xv == 0) && (ddt.v < 0)
+        ddt.xv = 0;
         ddt.v = 0;
-    elseif (x.x == 1) && (ddt.v > 0)
-        ddt.x = 0;
+    elseif (x.xv == 1) && (ddt.v > 0)
+        ddt.xv = 0;
         ddt.v = 0;
     end
 
@@ -44,7 +40,10 @@ function x = vec2struct(xvec, x, p)
     for i = 1:length(p.state_fields)
         x.(p.state_fields{i}) = [x.(p.state_fields{i}); xvec(i)];
     end
-    x.x(end) = min(max(x.x(end), 0), 1); % ~, valve fraction opening, limit between 0 and 1
+    x.xv(end) = min(max(x.xv(end), 0), 1); % ~, valve fraction opening, limit between 0 and 1
+
+    % Add intermediate variables to structure
+    x = intermediateVariables(x, u, d, fp, p);
 end
 
 function xvec = struct2vec(x, p)
