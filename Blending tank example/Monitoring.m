4c80af4b7cedd37a2b3d6bcc012d052de19e4e2c
tmlouw <tmlouw@CI507201.stb.sun.ac.za>
2022-09-22 23:48:05 +0200

    Implemented contained monitoring function

--- Blending tank example/Monitoring.m
+++ Blending tank example/Monitoring.m
@@ -1,14 +1,63 @@
-function [T, T2, SPE, faulty] = Monitoring(y, monitor)
-    X = [y.C.Data(end) y.C0.Data(end) y.F0.Data(end) y.FW.Data(end) y.F.Data(end) y.L.Data(end)];
-    X = (X - monitor.mX)./monitor.sX;
-    T = X*monitor.Q;
-    
-    T2 = T * monitor.iSig * T';
-    SPE = (X - T*monitor.Q') * (X - T*monitor.Q')';
-    
-    if (T2 > monitor.T2_threshold) || (SPE > monitor.SPE_threshold)
-        faulty = 1;
+function m = Monitoring(m, y, t)
+
+    if (m.Training) && (t.Time(end) >= m.trainingTime)
+        X = [];
+        for i = 1:length(m.y_fields)
+            f = m.y_fields{i};
+            X = [X y.(f).Data(110:end)']; % Remove the first part of transient data
+        end
+        
+        % Center and scale data
+        m.model.mX = mean(X);
+        m.model.sX = std(X);
+        X = (X - m.model.mX)./m.model.sX;
+        
+        % Train PCA model
+        [m.model.Q, Sig] = eigs((X' * X)/length(X), m.hyperparam.nComponents);
+        m.model.iSig = inv(Sig);
+        
+        % Monitoring statistics
+        T = X*m.model.Q;
+        T2 = diag(T * m.model.iSig * T');
+        SPE = diag((X - T*m.model.Q') * (X - T*m.model.Q')');
+        
+        m.statistic.T = T;
+        m.statistic.T2 = T2;
+        m.statistic.SPE = SPE;
+
+        m.Component.C.warning = zeros(1, length(m.T));
+        m.Component.C.alarm = false;
+
+        m.Training = false;
+        
     else
-        faulty = 0;
+        X = [];
+        for i = 1:length(m.y_fields)
+            f = m.y_fields{i};
+            X = [X y.(f).Data(end)]; 
+        end
+        X = (X - m.model.mX)./m.model.sX;
+        
+        % Monitoring statistics
+        T = X*m.model.Q;
+        T2 = diag(T * m.model.iSig * T');
+        SPE = diag((X - T*m.model.Q') * (X - T*m.model.Q')');
+        
+        if (T2 > m.hyperparam.T2_threshold) || (SPE > m.hyperparam.SPE_threshold)
+            m.Component.C.warning = [m.Component.C.warning 1];
+        else
+            m.Component.C.warning = [m.Component.C.warning 0];
+        end
+
+        if (sum(m.Component.C.warning(end-59:end)) / 60) > 0.8
+            m.Component.C.alarm = true;
+        end
+
+        m.statistic.T = [m.statistic.T T];
+        m.statistic.T2 = [m.statistic.T2 T2];
+        m.statistic.SPE = [m.statistic.SPE SPE];
+
     end
+
+
 end
