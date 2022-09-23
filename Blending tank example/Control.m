3c2889e8721f8546d1797a4ffcd0f6253c7923ed
tmlouw <tmlouw@CI507201.stb.sun.ac.za>
2022-09-23 02:26:33 +0200

    Simplified control valve fully-open and fully-closed code

--- Blending tank example/Control.m
+++ Blending tank example/Control.m
@@ -7,12 +7,8 @@ if r.components.valveFW.position == -1   % Apply control
     u.intError = u.intError + error*t.dt;
     u.xv = -u.PI.K*(error + u.intError/u.PI.tauI);
     
-elseif r.components.valveFW.position == 0    % Will be either 0 (closed) or 1 (open)
-    u.xv = 0;
-    u.intError = 0;
-
-elseif r.components.valveFW.position == 1    % Will be either 0 (closed) or 1 (open)
-    u.xv = 1;
+else   % Will be either 0 (closed) or 1 (open)
+    u.xv = r.components.valveFW.position;
     u.intError = 0;
 
 end
