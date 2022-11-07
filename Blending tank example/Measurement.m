ff7738cf2978216ea9d36d9ff89a5d4a1909e175
tmlouw <tmlouw@sun.ac.za>
2022-11-07 11:38:14 +0200

    Changed input argument from "fs" to "f"

--- Blending tank example/Measurement.m
+++ Blending tank example/Measurement.m
@@ -1,16 +1,16 @@
-function y = Measurement(y, x, d, fs, t)
+function y = Measurement(y, x, d, f, t)
     for i = 1: length(y.fields)
         cf = y.fields{i};
         current = y.(cf).function(t.time(end), x, d) + y.(cf).noiseVar*randn;
         
-        if strcmp(fs.(cf).state, 'Stuck')
+        if strcmp(f.(cf).state, 'Stuck')
             current = y.(cf).data(end); 
 
-        elseif strcmp(fs.(cf).state, 'Drift')
-            current = current + fs.(cf).drift;
+        elseif strcmp(f.(cf).state, 'Drift')
+            current = current + f.(cf).drift;
             
-        elseif strcmp(fs.(cf).state, 'Bias')
-            current = current + fs.(cf).bias;
+        elseif strcmp(f.(cf).state, 'Bias')
+            current = current + f.(cf).bias;
 
         end
 
