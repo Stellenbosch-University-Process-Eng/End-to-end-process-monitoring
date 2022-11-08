e8a0f7ab5193885d0999991e54c9a79d2a5afe10
tmlouw <tmlouw@sun.ac.za>
2022-11-08 22:41:07 +0200

    Implemented method to vary shutdown time based on number of compnents checked/replaced

--- Blending tank example/Maintenance.m
+++ Blending tank example/Maintenance.m
@@ -1,8 +1,9 @@
 function [r, f, t] = Maintenance(r, f, t)
     % Perform maintenance for unplanned / planned shut downs
-    ShutDownTime = 0;
+    ShutDownTime = r.MinimumShutDownTime;
 
-    % Cycle over all components
+    % Cycle over all components to identify components requiring
+    % replacement and to determine the total shutdown time
     for i = 1:length(r.components.fields)
         cf = r.components.fields{i}; % Current component field
             
@@ -10,21 +11,30 @@ function [r, f, t] = Maintenance(r, f, t)
         || strcmp(r.ShutType, r.components.(cf).type) % check components matching current planned maintenance type
             
             % Add the time taken to check component to shutdown time
-            ShutDownTime = ShutDownTime + r.components.(cf).
+            ShutDownTime = ShutDownTime + r.components.(cf).CheckComponentTime;
 
-            % if those components are also faulty (regardless of flagged)
-            ~strcmp(f.(cf).state, 'None'
-            f.(cf).state = 'None'; % Set the fault state to zero (replace component)
-            f.(cf).drift = 0; % Reset drift (if any)
-            r.components.(cf).faultFlag = false; % remove the fault flag
-            r.components.(cf).commision = t.time(end); % s, time when new component was commissioned
+            % Replace faulty components (regardless of flagged)
+            if ~strcmp(f.(cf).state, 'None')
+                f.(cf).state = 'Replace'; % Set the fault state to "Replace" (changed to "None" below)
+                ShutDownTime = ShutDownTime + r.components.(cf).ReplaceComponentTime;
+            end
         end
-
     end
     
     % Move time forward for the duration of the shutdown 
     % (proportional to number of inspections)
-    t.time(end) = t.time(end) + ShutDownPeriod;
+    t.time(end) = t.time(end) + ShutDownTime;
     
+    % Log the commision time and reset parameters for each replaced component as the time at
+    % which the plant is started up after the shutdown
+    for i = 1:length(r.components.fields)
+        cf = r.components.fields{i}; % Current component field
+        if strcmp(f.(cf).state, 'Replace') % All replaced components
+            f.(cf).state = 'None'; % Set the fault state to "None"
+            f.(cf).drift = 0; % Reset drift (if any)
+            r.components.(cf).faultFlag = false; % remove the fault flag
+            r.components.(cf).commision = t.time(end); % s, time when new component was commissioned
+        end
+    end
 
 end
