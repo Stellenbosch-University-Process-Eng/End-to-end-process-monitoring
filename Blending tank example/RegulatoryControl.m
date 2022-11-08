13b671664f743b8b81a021262d5039d80c82605c
tmlouw <tmlouw@sun.ac.za>
2022-11-08 22:31:35 +0200

    Minor changes to accommodate stuck valve

--- Blending tank example/RegulatoryControl.m
+++ Blending tank example/RegulatoryControl.m
@@ -6,9 +6,10 @@ if r.components.valveFW.position == -1   % Apply control
     error = r.setpoints.C(end) - y.C.data(end);
     u.intError = u.intError + error*t.dt;
     u.xv = -u.PI.K*(error + u.intError/u.PI.tauI);
-    
+    u.control = true;
 else   % Will be either 0 (closed) or 1 (open)
     u.xv = r.components.valveFW.position;
     u.intError = 0;
+    u.control = false;
 
 end
