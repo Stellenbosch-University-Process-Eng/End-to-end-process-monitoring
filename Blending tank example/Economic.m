648c47e229f6037bb1acedf1a3b77256c92a13cc
tmlouw <tmlouw@sun.ac.za>
2022-11-11 02:24:20 +0200

    Added costs to plant shutdowns

--- Blending tank example/Economic.m
+++ Blending tank example/Economic.m
@@ -1,4 +1,13 @@
 function econ = Economic(econ, r, x, t)
 
 
-econ.KPI.values(t.i + 1) = econ.KPI.function(r, x, t.i+1);
+
+if strcmp(r.regime, 'Shut')
+    if strcmp(r.ShutType, 'Unplanned')
+        econ.KPI.values(t.i + 1) = -250*r.ShutDownTime/3600;
+    else
+        econ.KPI.values(t.i + 1) = -100*r.ShutDownTime/3600;
+    end
+else
+    econ.KPI.values(t.i + 1) = econ.KPI.function(r, x, t.i+1);
+end
